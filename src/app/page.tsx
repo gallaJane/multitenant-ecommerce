d2075eef3c47299fcb4791cda84d941fe95c3f5a
elara\nudzejma.hanic <nudzejma.hanic@sme.additiv.com>
2025-06-03 14:56:58 +0200

    02: customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import { Button } from "@/components/ui/button";
-
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    <div>
-      <p className="text-rose-400">
-        Hello worlds
-
-      </p>
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant="elevated">I am Button</Button>
+        </div>
+        <div>
+          <Input placeholder="I'm an Input" />
+        </div>
+      </div>
     </div>
   );
 }
