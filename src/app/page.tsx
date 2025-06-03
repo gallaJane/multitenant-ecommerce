ad87ae1451400e732476a5b6ac1d7555152d5b0f
elara\nudzejma.hanic <nudzejma.hanic@sme.additiv.com>
2025-06-03 12:39:23 +0200

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-rose-400">
+        Hello worlds
+
+      </p>
+    </div>
+  );
+}
